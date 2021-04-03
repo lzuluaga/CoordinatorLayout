6ea992af87f603fada4e33074ae8bb1c4eae703e
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2021-04-03 09:00:41 -0500

    agregando toasts en los estados de la actividad

--- src/main/java/co/cedesistemas/myapplicationcedesistemas/MainActivity.kt
+++ src/main/java/co/cedesistemas/myapplicationcedesistemas/MainActivity.kt
@@ -2,12 +2,43 @@ package co.cedesistemas.myapplicationcedesistemas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "on create", Toast.LENGTH_SHORT).show()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "on start", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "on create", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "on pause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "on stop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "on destroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "on restart", Toast.LENGTH_SHORT).show()
+    }
 }
